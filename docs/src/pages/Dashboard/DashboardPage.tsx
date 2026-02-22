903d573702eaeea837a262ba093c6ba6df290ab9
Deepak Pandey <dpkpndy@hotmail.com>
2026-02-22 23:53:36 +0530

    Remove old docs build artifacts and frontend files from docs/

--- docs/src/pages/Dashboard/DashboardPage.tsx
+++
@@ -1,9 +0,0 @@
-import HeroSpotlight from "../../components/dashboard/HeroSpotlight";
-
-export default function DashboardPage() {
-  return (
-    <div className="space-y-8">
-      <HeroSpotlight />
-    </div>
-  );
-}
